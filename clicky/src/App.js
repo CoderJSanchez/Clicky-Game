120ba244dd5ae736dcbecbc1ad41aa3f97c9cb8e
Julio Sanchez <jsanchez191980802@gmail.com>
2019-02-10 20:15:49 -0600

    first commit

---
+++ clicky/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import MainNav from "./components/MainNav";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MainNav />
+      </div>
+    );
+  }
+}
+
+export default App;
