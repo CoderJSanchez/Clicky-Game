1d0f44de90351770b573f8c92f1ab233c4ede9d0
Julio Sanchez <jsanchez191980802@gmail.com>
2019-02-10 20:18:03 -0600

    init commit

--- clicky/src/components/MainNav.jsx
+++ clicky/src/components/MainNav.jsx
@@ -33,17 +33,6 @@ class MainNav extends Component {
                   GitHub
                 </NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
