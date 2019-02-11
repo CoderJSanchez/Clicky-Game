f94f5c7dc77f74243c0ba51f7ae1e09eeceb0e1e
Julio Sanchez <jsanchez191980802@gmail.com>
2019-02-10 23:53:01 -0600

    trying to map random images

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import MainNav from "./components/MainNav";
+import Image from "./components/Image";
+import data from "./data.js/imgData";
+
 import "./App.css";
+
+let randomImage = data[Math.floor(Math.random() * data.length)];
+console.log(randomImage.url);
+
+const imageComponent = data.map(pic => {
+  return (
+    <div className="col-md-3 mb-3" key={pic.id}>
+      <Image url={randomImage.url} />
+    </div>
+  );
+});
 
 class App extends Component {
   render() {
     return (
       <div>
         <MainNav />
+        <div className="conatiner text-center">
+          <div className="row mt-3">{imageComponent}</div>
+        </div>
       </div>
     );
   }
---
+++ clicky/src/data.js/imgData.js
@@ -0,0 +1,89 @@
+const images = [
+  {
+    albumId: 1,
+    id: 1,
+    title: "accusamus beatae ad facilis cum similique qui sunt",
+    url: "https://picsum.photos/200/300?image=0",
+    thumbnailUrl: "https://via.placeholder.com/150/92c952"
+  },
+  {
+    albumId: 1,
+    id: 2,
+    title: "reprehenderit est deserunt velit ipsam",
+    url: "https://picsum.photos/200/300?image=1",
+    thumbnailUrl: "https://via.placeholder.com/150/771796"
+  },
+  {
+    albumId: 1,
+    id: 3,
+    title: "officia porro iure quia iusto qui ipsa ut modi",
+    url: "https://picsum.photos/200/300?image=2",
+    thumbnailUrl: "https://via.placeholder.com/150/24f355"
+  },
+  {
+    albumId: 1,
+    id: 4,
+    title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+    url: "https://picsum.photos/200/300?image=3",
+    thumbnailUrl: "https://via.placeholder.com/150/d32776"
+  },
+  {
+    albumId: 1,
+    id: 5,
+    title: "natus nisi omnis corporis facere molestiae rerum in",
+    url: "https://picsum.photos/200/300?image=4",
+    thumbnailUrl: "https://via.placeholder.com/150/f66b97"
+  },
+  {
+    albumId: 1,
+    id: 6,
+    title: "accusamus ea aliquid et amet sequi nemo",
+    url: "https://picsum.photos/200/300?image=5",
+    thumbnailUrl: "https://via.placeholder.com/150/56a8c2"
+  },
+  {
+    albumId: 1,
+    id: 7,
+    title: "officia delectus consequatur vero aut veniam explicabo molestias",
+    url: "https://picsum.photos/200/300?image=6",
+    thumbnailUrl: "https://via.placeholder.com/150/b0f7cc"
+  },
+  {
+    albumId: 1,
+    id: 8,
+    title: "aut porro officiis laborum odit ea laudantium corporis",
+    url: "https://picsum.photos/200/300?image=7",
+    thumbnailUrl: "https://via.placeholder.com/150/54176f"
+  },
+  {
+    albumId: 1,
+    id: 9,
+    title: "qui eius qui autem sed",
+    url: "https://picsum.photos/200/300?image=8",
+    thumbnailUrl: "https://via.placeholder.com/150/51aa97"
+  },
+  {
+    albumId: 1,
+    id: 10,
+    title: "beatae et provident et ut vel",
+    url: "https://picsum.photos/200/300?image=9",
+    thumbnailUrl: "https://via.placeholder.com/150/810b14"
+  },
+  {
+    albumId: 1,
+    id: 11,
+    title: "nihil at amet non hic quia qui",
+    url: "https://picsum.photos/200/300?image=10",
+    thumbnailUrl: "https://via.placeholder.com/150/1ee8a4"
+  },
+  {
+    albumId: 1,
+    id: 12,
+    title:
+      "mollitia soluta ut rerum eos aliquam consequatur perspiciatis maiores",
+    url: "https://picsum.photos/200/300?image=11",
+    thumbnailUrl: "https://via.placeholder.com/150/66b7d2"
+  }
+];
+
+export default images;
